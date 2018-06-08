d632ca08c96381a4eacfc3b5f484070d26f74682
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-08 15:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20;
 
-    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge 
-rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
+    if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||(rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||(rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
   }
 }
 
